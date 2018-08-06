6321002adfaa476a5a234536a51fabe08e09d462
jeevan-jp <jiwanprakash65@gmail.com>
2018-08-06 16:32:21 +0530

    removed extra code in app.js

--- app.js
+++ app.js
@@ -77,10 +77,4 @@ $(document).ready(() => {
         $('#spinner').show();
         AQI.getAQI();
     });
-
-    function showSnackbar() {
-        var x = document.getElementById("snackbar");
-        x.className = "show";
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    }
 });
